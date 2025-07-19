340d2f612d5028c44edc5b5597dbb1de7ae92aae
Gledis Lami <lamigledi@gmail.com>
2025-07-19 10:36:49 +0200

    Initial commit

---
+++ src/main/java/org/sda/Main.java
@@ -0,0 +1,30 @@
+package org.sda;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException{
+        String url = "jdbc:mysql://localhost:3306/sda";
+        String username = "root";
+        String password = "root";
+
+        Connection connection = DriverManager.getConnection(url, username, password);
+        System.out.println("Connected succesfully!");
+
+        Statement statement = connection.createStatement();
+
+        int affectedRows = statement.executeUpdate("UPDATE students set department = 'Math' WHERE student_name = 'Gledis'");
+
+        System.out.println("Rows affected from update operation: "  + affectedRows);
+
+        ResultSet rs = statement.executeQuery("SELECT * FROM students");
+
+        // [ResultSetElement: {student_name = "Gledis", department = "IT"}, ResultSetElement: {"Ben", } ]
+
+        while (rs.next()){
+           String name = rs.getString("student_name");
+           String department = rs.getString("department");
+           System.out.println("Student name: " + name + ", department: " + department);
+        }
+    }
+}
